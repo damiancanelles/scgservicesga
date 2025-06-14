4a9eb6359eb048cda9edfeb3bc98d4496f5121d5
Damian Canelles Gómez <55611306+damiancanelles@users.noreply.github.com>
2025-06-14 11:26:51 -0400

    Add login and signup pages

---
+++ src/app/login/page.js
@@ -0,0 +1,9 @@
+import LoginPageComponent from "@/components/pages/login";
+
+export const metadata = {
+  title: "Login - SCG Services Atlanta",
+};
+
+export default function LoginPage() {
+  return <LoginPageComponent />;
+}
